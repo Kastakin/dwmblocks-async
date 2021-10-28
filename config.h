3a8d92b2a22666311bcffb5dfe341355ad6bfda8
Lorenzo Castellino <castellino96@gmail.com>
2021-10-28 21:00:22 +0200

    add battery module

--- config.h
+++ config.h
@@ -5,6 +5,7 @@
 
 const Block blocks[] = {
     /*     script       interval(s)  signal  */
-	BLOCK("sb-cpu",     5,            2)
+	BLOCK("sb-cpu",     5,            3)
+	BLOCK("sb-battery", 5,            2)
 	BLOCK("sb-date",    1,            1)
 };
