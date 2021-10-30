dc275a50e1c5dc0634a13d16e3c31f33fd532eae
Lorenzo Castellino <castellino96@gmail.com>
2021-10-30 15:46:06 +0200

    change delimiter spacing and block order

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 #define CMDLENGTH 50
-#define DELIMITER " "
+#define DELIMITER " | "
 #define LEADING_DELIMITER
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
     /*     script       interval(s)  signal  */
-	BLOCK("sb-cpu",     5,            3)
-	BLOCK("sb-battery", 5,            2)
-	BLOCK("sb-date",    1,            1)
+	//BLOCK("sb-cpu",     5,            3)
+	BLOCK("sb-bluetooth", 5,            3)
+	BLOCK("sb-battery",   2,            2)
+	BLOCK("sb-date",      1,            1)
 };
