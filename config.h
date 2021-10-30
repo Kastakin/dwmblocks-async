049aad989b63c47cdf0296b4bd697033c54424e4
Lorenzo Castellino <castellino96@gmail.com>
2021-10-30 16:00:33 +0200

    add network module

--- config.h
+++ config.h
@@ -6,7 +6,8 @@
 const Block blocks[] = {
     /*     script       interval(s)  signal  */
 	//BLOCK("sb-cpu",     5,            3)
-	BLOCK("sb-bluetooth", 5,            3)
+	BLOCK("sb-network",   2,            4)
+	BLOCK("sb-bluetooth", 2,            3)
 	BLOCK("sb-battery",   2,            2)
 	BLOCK("sb-date",      1,            1)
 };
